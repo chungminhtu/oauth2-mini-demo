a2ade8205862a1222f88fe7952f6e7c2974dc1f3
Chung Minh Tu <chungminhtu@gmail.com>
2024-08-07 10:34:26 +0800

    b

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const client = jwksClient({
 });
 
 const getKey = (header, callback) => {
+    
     client.getSigningKey(header.kid, (err, key) => {
         if (err) {
             return callback(err);
@@ -45,7 +46,6 @@ const verifyTokenWithJWKS = (token) => {
 const authenticateTokenJWKS = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
     if (token == null) return res.sendStatus(401);
 
     try {
