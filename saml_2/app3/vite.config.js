4d2874c5a73efdc72d0ae4fe602ee19b50ac147c
Chung Minh Tu <chungminhtu@gmail.com>
2025-07-22 13:51:19 +0800

    d

--- saml_2/app3/vite.config.js
+++ saml_2/app3/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     plugins: [react()],
+    root: './saml_2/app3',
     server: {
         port: 4003,
         open: true
     },
-    base: './',
 })
